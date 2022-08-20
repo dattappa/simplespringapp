7a14255279ea4d60436451a37aa265079ce89edd
=Narasimha <dattappa@outlook.com>
2022-08-20 21:28:29 +0530

    DDD

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello from Azure Spring Boot Application -Auto Trigger !";
+        return "Hello from Azure Spring Boot Application -Auto Trigger - again!";
     }
 }
